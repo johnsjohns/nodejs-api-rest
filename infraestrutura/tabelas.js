cd37880e225af728f7410fb95219f38a53796c18
johnsjohns <o-anao@hotmail.com>
2020-10-31 18:01:55 -0300

    Aula3.3

---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,21 @@
+class Tabelas{
+    init(conexao){
+        this.conexao = conexao;
+        this.criarAtendimento();
+    }
+
+    criarAtendimento(){
+        const sql = 'CREATE TABLE atendimento (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) not null, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))';
+
+        this.conexao.query(sql, erro => {
+            if(erro) {
+                console.log(erro);
+            }
+            else {
+                console.log('Tabela atendimento criada com sucesso');
+            }
+        });
+    }
+}
+
+module.exports = new Tabelas;
