598e843a52f00fdec3e3db734365b95b201d3dee
johnsjohns <o-ana@hotmail.com>
2020-10-30 22:19:21 -0300

    adicionado index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+});
+
+app.get('/atendimento', (req, res) => {
+    res.send('Você esta na rota de atendimento')
+});
