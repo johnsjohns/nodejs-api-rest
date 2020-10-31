16882405ff477d4ee3dbe064847382dc59292fc9
johnsjohns <o-ana@hotmail.com>
2020-10-30 22:57:33 -0300

    adicionado arquivos de configuraçao e de rotas

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const customExpress = require('./config/customExpress');
 
-const app = express();
+const app = customExpress();
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 });
 
-app.get('/atendimento', (req, res) => {
-    res.send('Você esta na rota de atendimento')
-});
+
